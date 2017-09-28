709bcef2ea9234130c3a91f677e6ded3c63a6cfe
Matthew Hunger <matt.hunger@gmail.com>
2017-09-28 00:43:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board 
+def display_board(game)
+  row_one = [" #{game[0]} ", "|", " #{game[1]} ", "|" " #{game[2]} ", ]
 end 
