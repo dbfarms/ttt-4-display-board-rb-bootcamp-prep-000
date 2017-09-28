ac381dd20b47f6984444df08626de079223cafe9
Matthew Hunger <matt.hunger@gmail.com>
2017-09-28 00:44:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,4 +3,8 @@
 def display_board(game)
   row_one = [" #{game[0]} ", "|", " #{game[1]} ", "|" " #{game[2]} "]
   row_two = "-----------"
+  row_three = [" #{game[3]} ", "|", " #{game[4]} ", "|" " #{game[5]} "]
+  row_four = "-----------"
+  row_five = [" #{game[6]} ", "|", " #{game[7]} ", "|" " #{game[8]} "]
+
 end
